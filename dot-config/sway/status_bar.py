d0fb320ed116e027afa4e5ecfc5f1e8d38dc0445
R Z <91019650+RZ781@users.noreply.github.com>
2026-09-17 22:19:30 +0100

    Replace swaybar text with icons

--- dot-config/sway/status_bar.py
+++ dot-config/sway/status_bar.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 import json, time, datetime, subprocess
 
+VOLUME_SYMBOLS = [(0, "󰕿"), (1, "󰖀"), (50, "󰕾")]
+BATTERY_SYMBOLS = [(i * 8, c) for i, c in enumerate("󰂎󰁺󰁻󰁼󰁽󰁾󰁿󰂀󰂁󰂂󰁹")]
+
 def text(s, width):
-    return {"full_text": f" {s.rjust(width)} "}
+    s = s.rjust(width)
+    s = "".join([c if c.isprintable() else f'<span font_family="SymbolsNerdFontMono">{c}</span>' for c in s])
+    return {"full_text": f" {s} ", "markup": "pango"}
+
+def get_symbol(symbols, number):
+    if isinstance(number, str):
+        number = int("0" + "".join([c for c in number if c.isdigit()]))
+    filtered = [(minimum, symbol) for minimum, symbol in symbols if number >= minimum]
+    return max(filtered)[1]
 
 def run(command):
     return subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True).stdout
@@ -23,7 +34,12 @@ def get_body():
         full_charge = int(f.read().strip())
     with open(f"{battery}/status") as f:
         state = f.read().strip().lower()
-    body.append(text(f"{round(charge/full_charge*100)}% ({state})", 18))
+    percent = round(charge / full_charge * 100)
+    if state == "discharging":
+        symbol = get_symbol(BATTERY_SYMBOLS, percent)
+    else:
+        symbol = "󰂄"
+    body.append(text(f"{percent}% {symbol}", 5))
 
     # volume
     get_volume = run(["pactl", "get-sink-volume", "@DEFAULT_SINK@"]).split()
@@ -31,11 +47,20 @@ def get_body():
     for field in get_volume:
         if field[-1] == "%":
             volume = field
-    get_muted = run(["pactl", "get-sink-mute", "@DEFAULT_SINK@"]).split()
-    muted = ""
+    get_muted = run(["pactl", "get-sink-mute", "@DEFAULT_SINK@"])
     if "yes" in get_muted:
-        muted = " (muted)"
-    body.append(text(f"{volume} volume{muted}", 18))
+        symbol = "󰝟"
+    else:
+        symbol = get_symbol(VOLUME_SYMBOLS, volume)
+    body.append(text(f"{volume} {symbol}", 5))
+
+    # microphone
+    get_muted = run(["pactl", "get-source-mute", "@DEFAULT_SOURCE@"])
+    if "yes" in get_muted:
+        symbol = "󰍭"
+    else:
+        symbol = "󰍬"
+    body.append(text(symbol, 1))
     return body
 
 header = {"version": 1}
